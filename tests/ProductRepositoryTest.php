ffd9d850a31b6f2447c0f8f96a1a9035aee1d065
Kacper <kscode.pl@gmail.com>
2022-07-23 21:19:24 +0200

    tests update

---
+++ tests/ProductRepositoryTest.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Tests;
+
+use App\Entity\Product;
+use Symfony\Bundle\FrameworkBundle\Test\KernelTestCase;
+
+class ProductRepositoryTest extends KernelTestCase
+{
+    /**
+     * @var \Doctrine\ORM\EntityManager
+     */
+
+    private $entityManager;
+
+    protected function testSetUp(): void
+    {
+        $kernel = self::bootKernel();
+
+        $this->entityManager = $kernel->getContainer()
+            ->get('doctrine')
+            ->getManager();
+    }
+
+    protected function testTearDown(): void
+    {
+        parent::tearDown();
+
+        $this->entityManager->close();
+        $this->entityManager = null;
+    }
+}
---
+++ tests/ResponsesTest.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Tests;
+
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+
+class ResponsesTest extends WebTestCase
+{
+    public function testLoginPageResponse(): void
+    {
+        $client = static::createClient();
+        $crawler = $client->request('GET', '/login');
+
+        $this->assertResponseIsSuccessful();
+        $this->assertSelectorTextContains('h1', 'Please sign in');
+    }
+
+    public function testRegisterPageResponse(): void
+    {
+        $client = static::createClient();
+        $crawler = $client->request('GET', '/register');
+
+        $this->assertResponseIsSuccessful();
+        $this->assertSelectorTextContains('h1', 'Register');
+    }
+}
